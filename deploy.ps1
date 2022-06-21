64eebdbfe2c2d8f6a9c87fabc1d54363694b3580
iloth <loth.istvan@hotmail.com>
2022-06-21 07:14:46 +0200

    deploy script: final

--- deploy.ps1
+++ deploy.ps1
@@ -1,54 +1,54 @@
 #clean destination
 Write-Output "####  clean destination"
-if (Test-Path -Path ".\distributable\web") {
-  Remove-Item -Path ".\distributable\web\*" -Recurse -Force
+if (Test-Path -Path ".\distributable") {
+  Remove-Item -Path ".\distributable\*" -Recurse -Force
 } else {
-  if (-not(Test-Path -Path ".\distributable\")) {
-    New-Item -Path ".\distributable\" -itemType Directory
-  }
-  if (-not(Test-Path -Path ".\distributable\web")) {
-    New-Item -Path ".\distributable\web" -itemType Directory
-  }
+  New-Item -Path ".\distributable" -itemType Directory
 }
 
 #build backend
-Write-Output "####  backend"
-Set-Location -Path .\source\backend
+Write-Output "####  build and copy backend"
+Remove-Item -Path ".\source\backend\dist\*" -Recurse -Force
+Set-Location -Path ".\source\backend"
 tsc --build --verbose
-Set-Location -Path ..\..
-Copy-Item -Path ".\source\backend\dist\*" -Destination ".\distributable\web" -Recurse
-Copy-Item -Path ".\source\backend\config" -Destination ".\distributable\web" -Recurse
-Copy-Item -Path ".\source\backend\package.json" -Destination ".\distributable\web"
-Copy-Item -Path ".\source\backend\src\swagger" -Destination ".\distributable\web" -Recurse
+Set-Location -Path "..\.."
+Copy-Item -Path ".\source\backend\dist\*" -Destination ".\distributable" -Recurse
+Copy-Item -Path ".\source\backend\config" -Destination ".\distributable" -Recurse
+Copy-Item -Path ".\source\backend\package.json" -Destination ".\distributable"
+Copy-Item -Path ".\source\backend\src\swagger" -Destination ".\distributable" -Recurse
 
 #build frontend
-Write-Output "####  frontend"
-Set-Location -Path .\source\frontend
+Write-Output "####  build and copy frontend"
+Set-Location -Path ".\source\frontend"
 ng build
-Set-Location -Path ..\..
-if (-not(Test-Path -Path ".\distributable\web\public")) {
-  New-Item -Path ".\distributable\web\public" -itemType Directory
+Set-Location -Path "..\.."
+if (-not(Test-Path -Path ".\distributable\public")) {
+  New-Item -Path ".\distributable\public" -itemType Directory
 }
-Copy-Item -Path ".\source\frontend\dist\*" -Destination ".\distributable\web\public" -Recurse
+Copy-Item -Path ".\source\frontend\dist\*" -Destination ".\distributable\public" -Recurse
 
-#build docker image
-Write-Output "####  docker image..."
-if (-not(Test-Path -Path ".\distributable\docker")) {
-  New-Item -Path ".\distributable\docker" -itemType Directory
+#docker image
+if (-not($args.Contains("-docker"))) {
+  Write-Output "####  build docker image"
+  docker build distributable -f .\dockerfile -t iloth-vizsgaremek
+
+  if ($args.Contains("docker")) {
+    Write-Output "####  run docker image"
+    docker run -d -p 8080:8080 --name iloth-vizsgaremek-run iloth-vizsgaremek
+    Start-Process "http://localhost:8080"
+  }
 }
-docker build distributable\web -f .\dockerfile -t iloth-vizsgaremek
 
-if ($args[0] -eq "docker") {
-  docker run -d -p 8080:8080 --name iloth-vizsgaremek-run iloth-vizsgaremek
-  Start-Process "http://localhost:8080"
-}
-
-#starting...
-if ($args[0] -eq "web") {
-  Write-Output "####  starting..."
-  Set-Location -Path .\distributable\web
+#nodejs
+if (-not($args.Contains("-node"))) {
+  Write-Output "####  install npm dependencies"
+  Set-Location -Path ".\distributable"
   npm i --omit=dev
-  node index.js
-  # Set-Location -Path ..\..
-  # Start-Process "http://localhost:8080"
+
+  if ($args.Contains("node")) {
+    Write-Output "####  start nodejs server"
+    Start-Process "http://localhost:8080"
+    node index.js
+  }
+  Set-Location -Path ".."
 }
